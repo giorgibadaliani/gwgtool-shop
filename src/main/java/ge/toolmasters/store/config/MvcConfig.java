0367fa6d281a01cd4720debddfe61bcf513b8583
giorgibadaliani <gbadaliani@gmail.com>
2026-03-02 00:48:20 +0400

    fix image url save

--- src/main/java/ge/toolmasters/store/config/MvcConfig.java
+++ src/main/java/ge/toolmasters/store/config/MvcConfig.java
@@ -3,6 +3,7 @@ package ge.toolmasters.store.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -11,13 +12,12 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // ეს იღებს შენი პროექტის ძირში არსებულ "uploads" ფოლდერის ზუსტ მისამართს
         Path uploadDir = Paths.get("uploads");
         String uploadPath = uploadDir.toFile().getAbsolutePath();
 
-        // 1. პროდუქტის ატვირთული სურათებისთვის ვქმნით ახალ გზას: /uploads/**
+        // ბრაუზერს ვეუბნებით: თუ მოგთხოვენ /uploads/ragaca.jpg, მოძებნე ის ჩვენს ფიზიკურ ფოლდერში!
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:/" + uploadPath + "/");
-
-        // (ხოლო /images/** უკვე ავტომატურად იმუშავებს static/images-დან)
     }
 }
